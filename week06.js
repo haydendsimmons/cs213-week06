f3745472f07650be8067ace8b9e7785225e6198e
haydendsimmons <haydendsimmons@gmail.com>
2018-05-30 23:58:34 -0600

    Update week06.js

--- week06.js
+++ week06.js
@@ -73,8 +73,31 @@ function dateValidate(fieldID, errorID)
     }
 }
 
-var statePattern = /^(?:A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|PA|RI|S[CD]|T[NX]|UT|V[AT]|W[AIVY])*$/;
+function stateValidate(fieldID, errorID)
+{
+    document.getElementById(fieldID).oninput = function () {
+        var state = document.getElementById(fieldID);
+        var stateVal = state.value;
+        var error = document.getElementById(errorID);
+        var valid = false;
+        var statePattern = /^(?:A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|PA|RI|S[CD]|T[NX]|UT|V[AT]|W[AIVY])*$/;
 
+        if (statePattern.test(stateVal) == true) {
+            valid = true;
+        }
+
+        else {
+            valid = false;
+        }
+
+        if (valid) {
+            error.style.display = 'none';
+        }
+        else {
+            error.style.display = 'inline';
+        }
+    }
+}
 
 ageValidate('age1', 'ageError1');
 ageValidate('age2', 'ageError2');
@@ -82,3 +105,5 @@ ssnValidate('ssn1', 'ssnError1');
 ssnValidate('ssn2', 'ssnError2');
 dateValidate('date1', 'dateError1');
 dateValidate('date2', 'dateError2');
+stateValidate('state1', 'stateError1');
+stateValidate('state2', 'stateError2');
